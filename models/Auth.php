acdec2c560d908a4bda67456a27785df714a08b3
Gabriel de Souza Scopel <93132666+Gabriel-Scopel@users.noreply.github.com>
2022-09-21 18:30:49 -0300

    debug login

--- models/Auth.php
+++ models/Auth.php
@@ -4,16 +4,18 @@ require_once 'dao/UserDaoMysql.php';
 class Auth{//autentica login do usuário
     private $pdo;
     private $base;
+    private $dao;
 
     public function __construct(PDO $pdo, $base){
         $this->pdo = $pdo;
         $this->base = $base;
+        $this->dao = new UserDaoMysql($this->pdo);
     }
     public function checkToken(){
         if(!empty($SESSION['token'])){
             $token = $_SESSION['token'];
-            $userDao = new UserDaoMysql($this->pdo);
-            $user = $userDao->findByToken($token);
+            
+            $user = $this->dao->findByToken($token);
             if($user){
                 return $user;
             }
@@ -21,14 +23,15 @@ class Auth{//autentica login do usuário
         header("location: ".$this->base."/login.php");
     }
     public function validateLogin($email, $password){
-        $userDao = new UserDaoMysql($this->pdo);
-        $user = $userDao->findByEmail($email);
+        
+        $user = $this->dao->findByEmail($email);
         if($user){
             if(password_verify($password, $user->password)){
+
                 $token = md5(time().rand(0,9999));
                 $_SESSION['token'] = $token;
                 $user -> token = $token;
-                $userDao->update($user);
+                $this->dao->update($user);
                 return true;
             }
         }
@@ -36,12 +39,12 @@ class Auth{//autentica login do usuário
 
     }
     public function emailExists($email){
-        $userDao = new UserDaoMysql($this->pdo);
-        return $userDao->findByEmail($email) ? true : false;
+        
+        return $this->dao->findByEmail($email) ? true : false;
     }
 
     public function registerUser($name, $email, $password, $birthdate){
-        $userDao = new UserDaoMysql($this->pdo);
+        
         $hash = password_hash($password, PASSWORD_DEFAULT);
         $token = md5(time().rand(0,9999));
 
@@ -53,7 +56,7 @@ class Auth{//autentica login do usuário
         $newUser->token = $token;
 
 
-        $userDao->insert($newUser);
+        $this->dao->insert($newUser);
         $_SESSION['token'];
     }
 }
